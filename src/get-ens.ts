67d5e604311764371fdd14c13b27bda88d598763
o-az <23618431+o-az@users.noreply.github.com>
2022-04-19 00:58:59 -0500

    replaced low quality cloudflare rpc

--- src/get-ens.ts
+++ src/get-ens.ts
@@ -15,6 +15,7 @@ export async function getENS(
 		to: resolver
 	})
 	if (error || !result) {
+		console.trace(error)
 		throw new Error(error?.message || "yikes, query failed")
 	}
 	const name = hexToString(result)
@@ -37,33 +38,46 @@ async function rpcRequest({
 	"result"?: string
 	"error"?: { "code": number; "message": string }
 }> {
-	const hashedAddress = await namehash(address)
-	const response = await fetch("https://cloudflare-eth.com/", {
+	const hashedAddress = namehash(address)
+	const response = await fetch("https://api.mycryptoapi.com/eth", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({
-			"jsonrpc": "2.0",
-			"method": "eth_call",
-			"id": 1,
-			"params": [{ "data": `${hashedMethodSig}${hashedAddress}`, to }, "latest"]
+			jsonrpc: "2.0",
+			method: "eth_call",
+			id: 2,
+			params: [{ "data": `${hashedMethodSig}${hashedAddress}`, to }, "latest"]
 		})
 	})
 	return await response.json()
 }
 
 async function getAvatar(name: string): Promise<string> {
-	const url = `https://metadata.ens.domains/mainnet/avatar/${name}`
-	const response = await fetch(url, { method: "HEAD" })
+	const response = await fetch(
+		`https://metadata.ens.domains/mainnet/avatar/${name}`,
+		{ method: "HEAD" }
+	)
 	if (response.status !== 200) {
 		return "There is no avatar set under given address"
 	}
 	return `https://metadata.ens.domains/mainnet/avatar/${name}`
 }
 
+// Different addresses can require different resolvers.
+async function getResolver(address: string) {
+	const { result, error } = await rpcRequest({
+		address,
+		hashedMethodSig: "0x0178b8bf",
+		to: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
+	})
+	if (error || !result) return ""
+	return `0x${result.slice(26, 66)}`
+}
+
 function hexToString(hex: string) {
 	let string = ""
 	for (let i = 0; i < hex.length; i += 2) {
-		string += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+		string += String.fromCharCode(parseInt(hex.slice(i, i + 2), 16))
 	}
 	// remove spaces and trailing null bytes and \u000
 	return string
@@ -73,52 +87,16 @@ function hexToString(hex: string) {
 		.trim()
 }
 
-/**
- * Different addresses can require different resolvers.
- * i.e., different "to" destination in the rpcRequest.
- * For example:
- *                address                                     resolver
- * 0x54c375c481f95ba43e2cEcd6Ef30631f55518f57 -> 0xa2c122be93b0074270ebee7f6b7292c7deb45047
- * 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045 -> 0x5fbb459c49bb06083c33109fa4f14810ec2cf358
- */
-async function getResolver(address: string) {
-	const { result, error } = await rpcRequest({
-		address,
-		hashedMethodSig: "0x0178b8bf",
-		to: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
-	})
-	if (error || !result) return ""
-	const resolver = `0x${result.slice(26, 66)}`
-	return resolver
-}
-
-async function namehash(address: string) {
+function namehash(address: string) {
 	let bytes = "00".repeat(32)
-	const normalized = normalizer(address)
-	const labels = ["reverse", "addr", normalized.slice(2).toLowerCase()]
+	const labels = ["reverse", "addr", normalizer(address).slice(2).toLowerCase()]
 	labels.map(label => {
-		const labelSha = keccak256(label)
-		const buffer = Buffer.from(bytes + labelSha, "hex")
+		const buffer = Buffer.from(bytes + keccak256(label), "hex")
 		bytes = keccak256(buffer)
 	})
 	return bytes
 }
-function normalizer(
-	name: string,
-	{
-		useStd3ASCII = true,
-		transitional = false
-	}: {
-		useStd3ASCII?: boolean
-		transitional?: boolean
-	} = {}
-) {
-	const nameNormalized = name.toLowerCase()
-	const nameNormalizedStd3ASCII = useStd3ASCII
-		? nameNormalized.normalize("NFKC")
-		: nameNormalized
-	const nameNormalizedTransitional = transitional
-		? nameNormalizedStd3ASCII.normalize("NFKC")
-		: nameNormalizedStd3ASCII
-	return nameNormalizedTransitional
+
+function normalizer(n: string) {
+	return n.toLowerCase().normalize("NFKC")
 }
